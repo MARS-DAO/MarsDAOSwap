3ccb12564e4e84020bc5b367dda13be552dbd272
DAOmars <97101757+DAOmars@users.noreply.github.com>
2022-02-25 12:04:21 +0200

    Update MarsDAO.sol

--- contracts/MarsDAO.sol
+++ contracts/MarsDAO.sol
@@ -7,7 +7,7 @@ import "./lib/ERC20.sol";
 
 contract MarsDAO is ERC20 {
 
-    constructor() public ERC20("MarsDAO", "MarsDAO") {
+    constructor() public ERC20("MarsDAO", "MDAO") {
         _mint(msg.sender, 100_000_000 * 1e18);
     }
 
